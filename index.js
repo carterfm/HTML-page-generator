e03494f0af266a8f86afcc84c44f1014c6b3b3f6
carterfm <carterf.morfitt@gmail.com>
2022-01-21 11:40:00 -0800

    Implemented file writing functionality

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-const htmlPrompt = inquirer
-    .prompt([
+const htmlPrompt = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             message: 'What is your name?',
@@ -33,3 +34,46 @@ const htmlPrompt = inquirer
             name: 'gitHub'
         }
 ]);
+}
+
+function generateHTML ( {username, location, dob, job, linkedIn, gitHub}) {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>${username}'s page</title>
+    </head>
+    <body>
+        <main class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1>Hello!</h1>
+                <h3>Welcome to ${username}'s page.</h3>
+                <ul class="list-group">
+                    <li class="list-group-item">Location: ${location}</li>
+                    <li class="list-group-item">Date of birth: ${dob}</li>
+                    <li class="list-group-item">Current professional position: ${job}</li>
+                    <li class="list-group-item">LinkedIn account: <a href="${linkedIn}" target="_blank" rel="noreferrer noopener">${linkedIn}</a></li>
+                    <li class="list-group-item">Github account: <a href="${gitHub}" target="_blank" rel="noreferrer noopener">${gitHub}</a></li>
+                  </ul>
+            </div>
+        </main>
+    </body>
+</html>`;
+
+}
+
+function init () {
+    htmlPrompt()
+        .then(data => fs.writeFile(`${data.username}.html`, generateHTML(data), (err) => {
+            if(err){
+              throw err;
+            }
+            console.log(`wrote ${data.username}.html`);
+        }))
+}
+
+init()
